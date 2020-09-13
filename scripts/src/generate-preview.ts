27fdd6b8bd184f8f2888b6788455316510e171d5
RanolP <public.ranolp@gmail.com>
2020-09-13 18:33:11 +0900

    Better escape

--- scripts/src/generate-preview.ts
+++ scripts/src/generate-preview.ts
@@ -3,10 +3,17 @@ import { readdir, writeFile, copyFile } from './util/fs';
 
 function escapeMarkdown(char: string): string {
   switch (char) {
+    // HTML Tag 구성 요소이므로 변환해야 함
     case '<':
       return '&lt;';
     case '>':
       return '&gt;';
+    // Typographer 기능 때문에 변환해야 함
+    case '\'':
+      return '&apos;';
+    case '"':
+      return '&quot;';
+    // 마크다운에서 문법적인 기능을 하므로 역슬래시 이스케이프
     case '\\':
     case '`':
     case '*':
@@ -25,6 +32,7 @@ function escapeMarkdown(char: string): string {
     case '~':
     case '|':
       return '\\' + char;
+    // 그 외의 경우는 그냥 반환함
     default:
       return char;
   }
@@ -44,7 +52,7 @@ export async function generatePreview(availableCharacters: Set<string>, donePerc
     for (const id of Array.from({ length: 256 }).map((_, index) => index)) {
       const charCode = (pageId << 8) | id;
       const character = String.fromCharCode(charCode);
-      let tag = '<div class="code tofu"></div>';
+      let tag = '<span class="code tofu"></span>';
       if (availableCharacters.has(character)) {
         tag = `<span class="character">${escapeMarkdown(character)}</span>`;
       }
