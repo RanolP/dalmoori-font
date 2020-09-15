4d91cab4b3c028e9270077cc104f18e362ca3a00
RanolP <public.ranolp@gmail.com>
2020-09-15 14:20:33 +0900

    List unsupported characters

--- scripts/src/generate-preview.ts
+++ scripts/src/generate-preview.ts
@@ -77,12 +77,15 @@ export async function generatePreview(availableCharacters: Set<string>, pageAvai
 
   console.log('Generating index');
 
+  const unsupported2350 = [...Requirements].filter(c => availableCharacters.has(c));
+
   const previewData = dedent`
   ---
   layout: home
   ---
   현대 한글 ${donePercentage.toFixed(2)}% 지원
-  KS X 1001에 실린 한글 2350 자, 한글 호환 자모, 아스키 문자 ${(100 * [...Requirements].filter(c => availableCharacters.has(c)).length / Requirements.length).toFixed(2)}% 지원
+  KS X 1001에 실린 한글 2350 자, 한글 호환 자모, 아스키 문자 ${(100 * unsupported2350.length / Requirements.length).toFixed(2)}% 지원
+  ${unsupported2350.join(', ')}
 
   ${[...pageAvailable].map(page => dedent`
     - [U+${page}00 ~ U+${page}FF]({{ site.baseurl }}/code/${page})  
