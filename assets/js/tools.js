f2f85dc00551271041dfe90a75d5cef49fe5d704
RanolP <public.ranolp@gmail.com>
2020-10-11 14:43:41 +0900

    To create something genuinely new

--- assets/js/tools.js
+++
@@ -1,9 +0,0 @@
-var isDalmooriEnabled = document.getElementById('is-dalmoori-enabled')
-
-isDalmooriEnabled.onchange = function () {
-  if (isDalmooriEnabled.checked) {
-    document.getElementsByTagName('body')[0].classList.add('dalmoori')
-  } else {
-    document.getElementsByTagName('body')[0].classList.remove('dalmoori')
-  }
-}
