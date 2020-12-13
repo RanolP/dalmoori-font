f590bf3b4f827384aaa63763042513fabd88d6df
RanolP <public.ranolp@gmail.com>
2020-12-14 01:10:58 +0900

    font

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,6 +1,11 @@
 import { css } from '@emotion/react';
 
 const Style = css`
+  @font-face {
+      font-family: 'dalmoori';
+      src: url('${process.env.PUBLIC_URL}/legacy/dalmoori.ttf') format('truetype');
+  }
+
   html {
     font-size: 16px;
   }
